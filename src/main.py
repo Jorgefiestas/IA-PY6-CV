8df8b89f6fe58c2a4bdad137125beffcbd244b57
Jorge Ignacio Fiestas Lavezzolo <jorge.fiestas@utec.edu.pe>
2020-12-15 17:03:55 -0500

    feat(main.py): Improves face detection, now it detects face regardless of it having mask or not

--- src/main.py
+++ src/main.py
@@ -1,21 +1,42 @@
-#Reconoce el rostro usando la cámara de la laptop
+import numpy as np
 import cv2
 
+# Models to detect faces in frames
+# Sources:
+# - https://github.com/opencv/opencv/blob/master/samples/dnn/face_detector/deploy.prototxt
+# - https://github.com/gopinath-balu/computer_vision/blob/master/CAFFE_DNN/res10_300x300_ssd_iter_140000.caffemodel
+
+model_address = '../models/deploy.protxt'
+weight_address = '../models/res10_300x300_ssd_iter_140000.caffemodel'
+net = cv2.dnn.readNet(model_address, weight_address)
+
 cap = cv2.VideoCapture(0)
-clasificador = cv2.CascadeClassifier('../cascades/haarcascade_frontalface_default.xml')
 
 while True:
-    ret, frame = cap.read()
-    imagenGris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    rostros = clasificador.detectMultiScale(imagenGris, 1.3, 8)
+	ret, frame = cap.read()
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),(104.0, 177.0, 123.0))
 
-    for (x,y,w,h) in rostros:
-        cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
+	(h, w) = frame.shape[:2]
 
-    cv2.imshow('frame', frame)
+	net.setInput(blob)
+	detections = net.forward()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	for i in range(0, detections.shape[2]):
+		confidence = detections[0, 0, i, 2]
+
+		if confidence > 0.9:
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+
+			(startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+			cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+
+	cv2.imshow('frame', frame)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
 cap.release()
 cv2.destroyAllWindows()
