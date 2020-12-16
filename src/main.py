f6cd796c414508fd86d6d0893f01c3187a2bc5dd
Martin Carrasco <martin.carrasco@utec.edu.pe>
2020-12-15 19:31:23 -0500

    feat(main.py): Addded module prediction to project

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import cv2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
 
 # Models to detect faces in frames
 # Sources:
@@ -10,44 +13,53 @@ faceModel = '../models/deploy.protxt'
 faceWeights = '../models/res10_300x300_ssd_iter_140000.caffemodel'
 faceNet = cv2.dnn.readNet(faceModel, faceWeights)
 
+classify_model = load_model("../models/nnet_mask")
 cap = cv2.VideoCapture(0)
 
 while True:
-	ret, frame = cap.read()
-	(h, w) = frame.shape[:2]
+    ret, frame = cap.read()
+    (h, w) = frame.shape[:2]
 
-	blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),(104.0, 177.0, 123.0))
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),(104.0, 177.0, 123.0))
 
-	faceNet.setInput(blob)
-	detections = faceNet.forward()
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
 
-	for i in range(0, detections.shape[2]):
-		confidence = detections[0, 0, i, 2]
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
 
-		# Check the detection threshold
-		if confidence < 0.9:
-			continue
+        # Check the detection threshold
+        if confidence < 0.9:
+                continue
 
-		# Finding the box that was detected
-		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-		(startX, startY, endX, endY) = box.astype("int")
+        # Finding the box that was detected
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
 
-		#(startX, startY) = (max(0, startX), max(0, startY))
-		#(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+        #(startX, startY) = (max(0, startX), max(0, startY))
+        #(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
 
-		face = image[startY:endY, startX:endX]
-		face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-		face = cv2.resize(face, (224, 224))
-		face = img_to_array(face)
-		face = preprocess_input(face)
-		face = np.expand_dims(face, axis=0)
+        face = frame[startY:endY, startX:endX]
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+        face = cv2.resize(face, (224, 224))
+        face = img_to_array(face)
+        face = preprocess_input(face)
+        face = np.expand_dims(face, axis=0)
 
-		cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+        (mask, without_mask) = classify_model.predict(face)[0]
+        label = "Mask" if mask > without_mask else "No mask" 
 
-	cv2.imshow('frame', frame)
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+        label = "{}: {:.2f}%".format(label, max(mask, without_mask) * 100)
 
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+        cv2.putText(frame, label, (startX, startY - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 cap.release()
 cv2.destroyAllWindows()
