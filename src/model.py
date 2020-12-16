7177375dd91e5d091b145592a8ae5223bced49b5
Martin Carrasco <martin.carrasco@utec.edu.pe>
2020-12-15 19:09:52 -0500

    fix(model.py): Organized model training for face mask detection

--- src/model.py
+++ src/model.py
@@ -20,102 +20,89 @@ import numpy as np
 import argparse
 import os
 
-INIT_LR = 1e-4
-EPOCHS = 20
-BS = 32
 
-data = []
-labels = []
-base_path = "../dataset/data/"
-for dir_path in os.listdir(base_path):
-    print(dir_path)
-    img_paths = list(paths.list_images( base_path + os.path.sep + dir_path))
-    for img_path in img_paths:
-        label = img_path.split(os.path.sep)[-2]
-        img = load_img(img_path, target_size=(224, 224))
-        img = img_to_array(img)
-        img = preprocess_input(img)
+def load_data(base_path):
+    data = []
+    labels = []
+    for dir_path in os.listdir(base_path):
+        print(dir_path)
+        img_paths = list(paths.list_images( base_path + os.path.sep + dir_path))
+        for img_path in img_paths:
+            label = img_path.split(os.path.sep)[-2]
+            img = load_img(img_path, target_size=(224, 224))
+            img = img_to_array(img)
+            img = preprocess_input(img)
 
-        data.append(img)
-        labels.append(label)
-data = np.array(data, dtype="float32")
-labels = np.array(labels)
+            data.append(img)
+            labels.append(label)
+    data = np.array(data, dtype="float32")
+    labels = np.array(labels)
 
-lb = LabelBinarizer()
-labels = lb.fit_transform(labels)
-labels = to_categorical(labels)
-
-(trainX, testX, trainY, testY) = train_test_split(data, labels,
-	test_size=0.20, stratify=labels, random_state=42)
-
-aug = ImageDataGenerator(
-	rotation_range=20,
-	zoom_range=0.15,
-	width_shift_range=0.2,
-	height_shift_range=0.2,
-	shear_range=0.15,
-	horizontal_flip=True,
-	fill_mode="nearest")
-
-# load the MobileNetV2 network, ensuring the head FC layer sets are
-# left off
-baseModel = MobileNetV2(weights="imagenet", include_top=False,
-	input_tensor=Input(shape=(224, 224, 3)))
-# construct the head of the model that will be placed on top of the
-# the base model
-headModel = baseModel.output
-headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
-headModel = Flatten(name="flatten")(headModel)
-headModel = Dense(128, activation="relu")(headModel)
-headModel = Dropout(0.5)(headModel)
-headModel = Dense(2, activation="softmax")(headModel)
-# place the head FC model on top of the base model (this will become
-# the actual model we will train)
-model = Model(inputs=baseModel.input, outputs=headModel)
-# loop over all layers in the base model and freeze them so they will
-# *not* be updated during the first training process
-for layer in baseModel.layers:
-	layer.trainable = False
+    return data, labels
 
 
-# compile our model
-print("[INFO] compiling model...")
-opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss="binary_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+def train_model(data, labels, EPOCHS = 1, BS = 32, LR=1e-4):
+    lb = LabelBinarizer()
+    labels = lb.fit_transform(labels)
+    labels = to_categorical(labels)
 
-# train the head of the network
-print("[INFO] training head...")
-H = model.fit(
-	aug.flow(trainX, trainY, batch_size=BS),
-	steps_per_epoch=len(trainX) // BS,
-	validation_data=(testX, testY),
-	validation_steps=len(testX) // BS,
-	epochs=EPOCHS)
+    (X_train, X_test, Y_train, Y_test) = train_test_split(data, labels,
+            test_size=0.20, stratify=labels, random_state=42)
 
-# make predictions on the testing set
-print("[INFO] evaluating network...")
-predIdxs = model.predict(testX, batch_size=BS)
-# for each image in the testing set we need to find the index of the
-# label with corresponding largest predicted probability
-predIdxs = np.argmax(predIdxs, axis=1)
-# show a nicely formatted classification report
-print(classification_report(testY.argmax(axis=1), predIdxs,
-	target_names=lb.classes_))
-# serialize the model to disk
-print("[INFO] saving mask detector model...")
-model.save(args["model"], save_format="h5")
+    aug = ImageDataGenerator(
+            rotation_range=20,
+            zoom_range=0.15,
+            width_shift_range=0.2,
+            height_shift_range=0.2,
+            shear_range=0.15,
+            horizontal_flip=True,
+            fill_mode="nearest")
 
-# plot the training loss and accuracy
-N = EPOCHS
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, N), H.history["accuracy"], label="train_acc")
-plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend(loc="lower left")
-plt.savefig(args["plot"])
+    std_model = MobileNetV2(weights="imagenet", include_top=False,
+            input_tensor=Input(shape=(224, 224, 3)))
+
+    head_model = std_model.output
+    head_model = AveragePooling2D(pool_size=(7, 7))(head_model)
+    head_model = Flatten(name="flatten")(head_model)
+    head_model = Dense(128, activation="relu")(head_model)
+    head_model = Dropout(0.5)(head_model)
+    head_model = Dense(2, activation="softmax")(head_model)
+
+    model = Model(inputs=std_model.input, outputs=head_model)
+    for layer in std_model.layers:
+            layer.trainable = False
+
+
+    opt = Adam(lr=LR, decay=LR / EPOCHS)
+    model.compile(loss="binary_crossentropy", optimizer=opt,
+            metrics=["accuracy"])
+
+    fitted_model = model.fit(
+            aug.flow(X_train, Y_train, batch_size=BS),
+            steps_per_epoch=len(X_train) // BS,
+            validation_data=(X_test, Y_test),
+            validation_steps=len(X_test) // BS,
+            epochs=EPOCHS)
+
+    f_idx = model.predict(X_test, batch_size=BS)
+    f_idx = np.argmax(f_idx, axis = 1)
+    model.save("model_cnn", save_format="h5")
+    return fitted_model
+
+def plot_curve(model, N):
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(np.arange(0, N), model.history["loss"], label="train_loss")
+    plt.plot(np.arange(0, N), model.history["val_loss"], label="val_loss")
+    plt.plot(np.arange(0, N), model.history["accuracy"], label="train_acc")
+    plt.plot(np.arange(0, N), H.history["val_accuracy"], label="val_acc")
+    plt.title("Learning curve")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss vs Accuracy")
+    plt.legend(loc="lower left")
+    plt.savefig("learning_curve.png")
+
+
+data, labels = load_data("../dataset/data/")
+fitted_model = train_model(data = data, labels = labels, EPOCHS = 5)
+plot_curve(fitted_model, N = 5)
